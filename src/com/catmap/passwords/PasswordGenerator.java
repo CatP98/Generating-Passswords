464f2f51fb8f5f8dbfc9e23098c4568aea345cc9
catarina p <catarinampestana@ua.pt>
2024-11-30 23:37:12 +0000

    Check if all the specifications the user asked for (lowercase, uppercase, digits or symbols, are met at least once, if not re-generate another pass

--- src/com/catmap/passwords/PasswordGenerator.java
+++ src/com/catmap/passwords/PasswordGenerator.java
@@ -5,22 +5,21 @@ import java.util.Random;
 public class PasswordGenerator {
 
     String generatePassword(int length, boolean useLowerCase, boolean useUpperCase,
-                            boolean useNumbers, boolean useSpecialChars){
+                            boolean useNumbers, boolean useSpecialChars) {
 
         String lowercase = "abcdefghijkmlnopqrstuvwxyz";
         String upperCase = "ABCDEFGHIJKLMNOPQRSTUWVYZ";
         String digits = "0123456789";
         String symbols = "!@#$%^&*()_-[]{}";
 
-        StringBuilder charOptions = new StringBuilder();
-        StringBuilder password = new StringBuilder();
         Random rand = new Random();
+        StringBuilder charOptions = new StringBuilder();
 
-        if(useLowerCase){
+        if (useLowerCase) {
             charOptions.append(lowercase);
         }
 
-        if( useUpperCase){
+        if (useUpperCase) {
             charOptions.append(upperCase);
         }
 
@@ -34,10 +33,52 @@ public class PasswordGenerator {
 
         String allChars = charOptions.toString();
 
-        for(int i = 0; i < length; i++){
-            char nextLetter = allChars.charAt(rand.nextInt(allChars.length()));
-            password.append(nextLetter);
+        boolean passwordIsValid = false;
+
+        while (true) {
+
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                char nextLetter = allChars.charAt(rand.nextInt(allChars.length()));
+                password.append(nextLetter);
+            }
+            String pass = password.toString();
+
+            passwordIsValid = true;
+
+            if(useLowerCase && !hasOverlap(pass, lowercase)){
+                passwordIsValid = false;
+            }
+
+            if(useUpperCase && !hasOverlap(pass, upperCase)){
+                passwordIsValid = false;
+            }
+
+            if(useSpecialChars && !hasOverlap(pass, symbols)){
+                passwordIsValid = false;
+            }
+
+            if(useNumbers && !hasOverlap(pass, digits)){
+                passwordIsValid = false;
+            }
+
+            if(useLowerCase && !hasOverlap(pass, lowercase)){
+                passwordIsValid = false;
+            }
+            if(passwordIsValid){
+                return pass;
+            }
+
+            System.out.println("Password failed, pass: " + pass);
         }
-        return password.toString();
+    }
+
+    private boolean hasOverlap(String s1, String s2){
+        for(char c : s1.toCharArray()){
+            if(s2.contains(String.valueOf(c))){
+                return true;
+            }
+        }
+        return false;
     }
 }
